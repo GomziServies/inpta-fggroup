44c6b756517eaba37c263e9f65fc9aa06084a724
Brijesh8866 <brijesh8866p@gmail.com>
2025-02-06 17:49:50 +0530

    update: add listing design add tp and tc

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import AllListing from "./pages/all-listing";
 import ListingView from "./pages/view-listing";
 import UpdateListing from "./pages/update-listing";
 import ViewAllListing from "./pages/view-all-listing";
+import TPRegistrationListing from "./pages/training-and-placement";
+import TCRegistrationListing from "./pages/training-center";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
         <Route path="/view-listing" element={<ListingView />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/add-listing" element={<AddListing />} />
+        <Route path="/training-and-placement" element={<TPRegistrationListing />} />
+        <Route path="/training-center" element={<TCRegistrationListing />} />
         <Route path="/update-listing" element={<UpdateListing />} />
         <Route path="/view-all-listing" element={<ViewAllListing />} />
       </Routes>
